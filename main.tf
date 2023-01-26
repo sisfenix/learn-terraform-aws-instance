68597adde2e5e5cf7c66481050e9c5b8ee5de110
Alan Lopes <desenv.alanlopes@gmail.com>
2023-01-25 22:00:27 -0300

    enviando comando via user_data

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "IaC-AWS"
 
+  user_data = <<-EOF
+    #! /bin/bash
+    cd /home/ubuntu/
+    echo "<h1>Deploy via terraform</h1>" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+  user_data_replace_on_change = true
+
   tags = {
     Name = "ExampleAppServerInstance"
   }
