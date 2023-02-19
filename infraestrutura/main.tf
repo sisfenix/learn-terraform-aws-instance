1fea3c5fb76699d595c149913cd5533477e41d0c
Alan Lopes <desenv.alanlopes@gmail.com>
2023-02-18 23:31:25 -0300

    configurando o autoscaling com lb

--- infraestrutura/main.tf
+++ infraestrutura/main.tf
@@ -37,12 +37,58 @@ output "IP_Publico" {
 }
 
 resource "aws_autoscaling_group" "grupoAS" {
-  availability_zones = ["${var.regiao_aws}a"]
+  availability_zones = ["${var.regiao_aws}a", "${var.regiao_aws}a"]
   name               = var.nomeGrupoAS
   max_size           = var.maximoGrupoAS
   min_size           = var.minimoGrupoAS
   launch_template {
     id      = aws_launch_template.maquina.id
     version = "$Latest"
+  }
+  target_group_arns = [aws_lb_target_group.lb_target_group.arn]
+}
+
+resource "aws_default_subnet" "subnet_a" {
+  availability_zone = "${var.regiao_aws}a"
+}
+
+resource "aws_default_subnet" "subnet_b" {
+  availability_zone = "${var.regiao_aws}b"
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets  = [aws_default_subnet.subnet_a.id, aws_default_subnet.subnet_b.id]
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name     = "maquinasAlvo"
+  port     = "8000"
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_lb_listener" "listenerLoadBalance" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port              = "8000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}
+
+resource "aws_autoscaling_policy" "pol_autoscaling_producao" {
+  name                   = "terraform-autoscaling"
+  autoscaling_group_name = var.nomeGrupoAS
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
   }
 }
